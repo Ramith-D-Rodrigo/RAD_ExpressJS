00e1333ce7045b194216af75fea605445e15a4e2
Ramith-D-Rodrigo <ramithrodrigo@yahoo.com>
2022-08-20 13:44:47 +0530

    Create second_file.js

---
+++ second_file.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res){
+    res.send('Get route on second_file');
+});
+
+router.post('/', function(req, res){
+    res.send('POST route on second_file');
+});
+
+module.exports = router;
